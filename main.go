4863246228e421051eeecac762018e8810e61f54
Neil Kuan <guan840912@gmail.com>
2024-01-31 09:10:26 +0800

    chore: force sleep 65s for graceful shutdown

--- main.go
+++ main.go
@@ -18,30 +18,36 @@ func main() {
 		Handler: r,
 	}
 
+	idleConnsClosed := make(chan struct{})
+
 	// Initializing the server in a goroutine so that
 	// it won't block the graceful shutdown handling below
 	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal, 1)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutting down server...")
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		quit := make(chan os.Signal, 1)
+		// kill (no param) default send syscall.SIGTERM
+		// kill -2 is syscall.SIGINT
+		// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+		<-quit
+		log.Println("Shutting down server...")
 
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 65*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown: ", err)
+		// The context is used to inform the server it has 5 seconds to finish
+		// the request it is currently handling
+		time.Sleep(65 * time.Second)
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Fatal("Server forced to shutdown: ", err)
+		}
+		close(idleConnsClosed)
+		log.Println("Server exiting")
+
+	}()
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("listen: %s\n", err)
 	}
 
-	log.Println("Server exiting")
+	<-idleConnsClosed
+
 }
